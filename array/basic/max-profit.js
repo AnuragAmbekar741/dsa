0dcffb36adf09f3be707084d0814c472529a173b
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-02-03 23:00:33 -0500

    comments updated

--- array/basic/max-profit.js
+++ array/basic/max-profit.js
@@ -7,8 +7,6 @@
 function maxProfit(prices) {
     let n = prices.length;
     let res = 0;
-
-    // Explore all possible ways to buy and sell stock
     for (let i = 0; i < n - 1; i++) {
         for (let j = i + 1; j < n; j++) {
             res = Math.max(res, prices[j] - prices[i]);
@@ -23,16 +21,10 @@ console.log(maxProfit(prices));
 function maxProfit2(prices) {
     let minSoFar = prices[0];
     let res = 0;
-
-    // Update the minimum value seen so far 
-    // if we see smaller
     for (let i = 1; i < prices.length; i++) {
         minSoFar = Math.min(minSoFar, prices[i]);
-
-        // Update result if we get more profit                
         res = Math.max(res, prices[i] - minSoFar);
     }
-
     return res;
 }
 
