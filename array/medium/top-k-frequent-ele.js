f472674ddc3ecc336d936eed0f770a2a56808820
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-02-10 00:30:44 -0500

    top k frequency ele

--- array/medium/top-k-frequent-ele.js
+++ array/medium/top-k-frequent-ele.js
@@ -1,25 +1,22 @@
-function topKFrequentEle(nums, freq) {
-    if (nums.length === 0) return nums;
-
-    const countMap = new Map();
-    for (const num of nums) {
-        countMap.set(num, (countMap.get(num) || 0) + 1);
+function topKFrequencyEle(nums, k) {
+    if (nums?.length === 0) return nums
+    const countMap = new Map()
+    for (let num of nums) {
+        countMap.set(num, (countMap.get(num) || 0) + 1)
     }
-
-    const priorityQue = [];
-
-    const compare = (a, b) => countMap.get(a) - countMap.get(b);
-
-    for (const num of countMap.keys()) {
-        priorityQue.push(num);
-        priorityQue.sort(compare);
-        if (priorityQue.length > freq) {
-            priorityQue.shift();
+    const topK = []
+    for (let num of countMap.keys()) {
+        topK.push(num)
+        topK.sort((a, b) => countMap.get(a) - countMap.get(b))
+        if (topK.length > k) {
+            topK.shift()
         }
     }
-
-    return priorityQue;
+    return topK
 }
 
-console.log(topKFrequentEle([1, 1, 1, 2, 2, 3], 2)); // Output: [1, 2]
-console.log(topKFrequentEle([4, 4, 4, 4, 5, 5, 5, 6, 6, 7], 3)); // Output: [4, 5, 6]
+console.log(topKFrequencyEle([1, 1, 1, 2, 2, 3], 2)); // Output: [1, 2]
+console.log(topKFrequencyEle([4, 4, 4, 4, 5, 5, 5, 6, 6, 7], 3)); // Output: [4, 5, 6]
+console.log(topKFrequencyEle([1], 1)); // Output: [1]
+console.log(topKFrequencyEle([1, 2, 3, 4, 5], 2)); // Output: [4, 5] (or any two highest occurring)
+console.log(topKFrequencyEle([], 3)); // Output: []
