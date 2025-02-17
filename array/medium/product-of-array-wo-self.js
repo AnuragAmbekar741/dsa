928a6c3f0abea8fce23531daaddc20672424aa76
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-02-17 01:26:36 -0500

    product of arr - optimised solution

--- array/medium/product-of-array-wo-self.js
+++ array/medium/product-of-array-wo-self.js
@@ -2,9 +2,31 @@
 // with % operator
 function prouctofArray(arr) {
     let n = arr.length;
-    const arrProd = arr.reduce((acc, curr) => acc * curr, 1);
-    const newArr = Array(n).fill(arrProd);
-    return newArr.map((ele, i) => ele / arr[i]);
+    const prodArr = arr.map(ele => arr.reduce((acc, val) => acc * val) / ele);
+    return prodArr;
 }
 
+
 console.log(prouctofArray([1, 2, 3, 4])); // Output: 24
+
+// o(n) without / operator
+function productOfArrayWithoutSelf(arr) {
+    let prefix = 1, postfix = 1
+
+    const result = Array(arr.length).fill(1);
+
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = prefix;
+        prefix *= arr[i]
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        result[i] *= postfix;
+        postfix *= arr[i];
+    }
+
+    return result;
+}
+console.log(productOfArrayWithoutSelf([1, 2, 3, 4])); // Output: 24
+
+
